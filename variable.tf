4e74d8cd75211ead842e61e9d23af7ee4f4caee3
Praveen Pulluru <pulluru.praveen@gmail.com>
2025-06-23 17:28:35 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,2 @@
+.rewritePath("/trademark/cms/rest/case/(?<sn>\\d{8})\\/(?<doctype>[^/]+)\\/(?<filename>[^/]+)$", "/cases/${sn}/MRK/${filename}".startsWith("/") ? "/cases/${sn}/MRK/${filename}" : "/cases/${sn}/MRK/${filename}")
 .rewritePath("/trademark/cms/rest/case/(?<sn>\\d{8})\\/(?<doctype>[^/]+)\\/(?<filename>[^/]+)$", "/cases/${sn}/MRK/${filename}")
