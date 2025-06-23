64bf110bab675ee4493b9a49a4c3e0b22146825a
Praveen Pulluru <pulluru.praveen@gmail.com>
2025-06-23 15:27:41 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,22 @@
-{
-  "timestamp": "2025-06-23T19:08:15.276+00:00",
-  "path": "/trademark/cms/rest/case/76900900/mark/MRK_00.jpg%20%22documentName%22:%20%22MRK_00.jpg%22,%22documentAlias%22:%20%22mark%22,%22createdByUserId%22:%20%22eFile%22,%22accessLevel%22:%20%22public%22,%22documentType%22:%20%22mark%22,%22docCode%22:%20%22MRK%22,%22mailDate%22:%20%222025-06-10T14:45:18.426-0400%22,%20%22loadDate%22:%20%222025-06-10T14:45:18.426-0400%22,%20%22scanDate%22:%20%222025-06-10T14:45:18.426-0400%22,%20%22effectiveStartDate%22:%20%222025-06-10T14:45:18.426-0400%22,%20%22modifiedByUserId%22:%20%22Teas%22,%20%22migrationSource%22:%20%22TCIM%22,%20%22migrationMethod%22:%20%22upload%22,%22sourceMedium%22:%20%22EMAIL%22,%22sourceMedia%22:%20%22ELECTRONIC%22%7Dhttp://localhost:8081/trademark/cms/rest/case/76900900/mark/MRK_00.jpg",
-  "status": 500,
-  "error": "Internal Server Error",
-  "requestId": "e2e328b2-1"
-}
+PUT -> http://localhost:8081/tdk/cms/rest/case/76900900/mark/MRK_00.jpg
+content -> uploaded MRK_00.jpg
 
-JsonNode rootNode = objectMapper.readTree(payload);
+metadata -> 
+
+{ 
+"documentName": "MRK_00.jpg",
+"documentAlias": "mark",
+"createdByUserId": "eFile",
+"accessLevel": "public",
+"documentType": "mark",
+"docCode": "MRK",
+"mailDate": "2025-06-10T14:45:18.426-0400", 
+"loadDate": "2025-06-10T14:45:18.426-0400", 
+"scanDate": "2025-06-10T14:45:18.426-0400", 
+"effectiveStartDate": "2025-06-10T14:45:18.426-0400", 
+"modifiedByUserId": "test", 
+"migrationSource": "ttt", 
+"migrationMethod": "upload",
+"sourceMedium": "EMAIL",
+"sourceMedia": "ELECTRONIC"
+}
