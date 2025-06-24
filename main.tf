45c38ecea044a5e1df50f96bae406199c4e7d453
Praveen Pulluru <pulluru.praveen@gmail.com>
2025-06-24 13:53:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,68 +1,32 @@
-.filters(f -> f.filter((exchange, chain) -> {
-    String path = exchange.getRequest().getPath().toString();
-    AntPathMatcher matcher = new AntPathMatcher();
-    Map<String, String> variables = matcher.extractUriTemplateVariables(PATH_PATTERN, path);
+.route("route-put-mark-documents", r -> r
+    .path("/trademark/cms/rest/case/**")
+    .and()
+    .method("PUT")
+    .filters(f -> f
+        .filter(rewritePathFilter) // Apply path rewrite first
+        .filter((exchange, chain) ->
+            metadataMatcher.matchWithPredicate(exchange, "documentType", val -> "mark".equals(val))
+                .flatMap(match -> {
+                    if (match) {
+                        URI updatedUri = exchange.getRequest().getURI();
+                        String rewrittenPath = updatedUri.getPath(); // after rewrite
+                        URI newUri = UriComponentsBuilder.fromUri(updatedUri)
+                            .scheme("https")
+                            .host(URI.create(cloudUrl).getHost())
+                            .port(URI.create(cloudUrl).getPort())
+                            .replacePath(rewrittenPath)
+                            .build(true)
+                            .toUri();
 
-    String caseId = variables.get("caseId");
-    String fileName = variables.get("fileName");
+                        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                            .uri(newUri)
+                            .build();
 
-    if (!fileName.startsWith("MRK_")) {
-        logger.warn("Rejecting upload: fileName '{}' does not start with 'MRK_'", fileName);
-        exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
-        return exchange.getResponse().setComplete();
-    }
-
-    logger.info("Accepted file upload for caseId={}, fileName={}", caseId, fileName);
-
-    exchange.getRequest().mutate()
-        .header("X-Case-Id", caseId)
-        .header("X-File-Name", fileName)
-        .build();
-
-    return chain.filter(exchange);
-}, 10100))
-
-
-
-
-
-
-
-
-
-
-@Component
-public class DocumentTypeRoutePredicateFactory extends AbstractRoutePredicateFactory<DocumentTypeRoutePredicateFactory.Config> {
-
-    public DocumentTypeRoutePredicateFactory() {
-        super(Config.class);
-    }
-
-    @Override
-    public Predicate<ServerWebExchange> apply(Config config) {
-        return exchange -> {
-            String path = exchange.getRequest().getPath().toString();
-            return path.contains("/" + config.documentType + "/");
-        };
-    }
-
-    public static class Config {
-        private String documentType;
-
-        public String getDocumentType() {
-            return documentType;
-        }
-
-        public void setDocumentType(String documentType) {
-            this.documentType = documentType;
-        }
-    }
-}
-
-
-
-.route("mark_upload", r -> r
-    .path(PATH_PATTERN)
-    .predicate(new DocumentTypeRoutePredicateFactory().apply(c -> c.setDocumentType("mark")))
-    .filters(...)
-    .uri(...))
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    }
+                    return chain.filter(exchange);
+                })
+        )
+    )
+    .uri(cloudUrl)
+)
